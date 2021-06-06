76becc973a1b6694f68907c5448e3d8b8cf5bf56
suyashagr000 <85409757+suyashagr000@users.noreply.github.com>
2021-06-06 15:54:53 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing <strong>BOOTCAMP</strong> taken by
+        <strong>
+          <em> Mr. Shaurya Sinha.</em>
+        </strong>{" "}
+        We covered everything from Scratch including Javascript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
